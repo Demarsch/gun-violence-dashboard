5a9c856d79a2ca4b9036ad3e456454590856f61b
Andrei Chaplygin <gotacoin@gmail.com>
2018-11-30 09:28:26 -0800

    Added ability to close widget and a generic modal window

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,9 @@
 widgetRegistry = [];
 
+$('#addWidget').click(() => {
+    $('#modal').modal();
+});
+
 function createWidget(x, y, width, height, title) {
     let widget = $(document.createElement('div'))
         .addClass('grid-stack-item')
@@ -9,9 +13,14 @@ function createWidget(x, y, width, height, title) {
         .attr('data-gs-height', height)
         .html(`
         <div class="grid-stack-item-content">
-            <h2>${title}</h2>
+            <div class="widget-header">    
+                <h2>${title}</h2>
+                <a class="st-widget-btn" title="Settings" href="#"><img src="static/img/gear.svg"></a>
+                <a class="rm-widget-btn" title="Delete Widget" href="#"><img src="static/img/close.svg"></a>
+            </div>
         </div>
         `);
+    widget.find('.rm-widget-btn').click(() => widget.remove());
     $('.grid-stack').append(widget);
 }
 
