d23e748408b9a6635fa05c4f758c353cb3dd544a
Craig Taylor <craigtaylor86@hotmail.com>
2018-12-06 19:36:08 -0800

    build Plot done?

--- static/js/build_Plot.js
+++ static/js/build_Plot.js
@@ -1,15 +1,18 @@
-
-
-
-function build_3_axis(data) {    
-        //let input_dataX = data.xValue;
-        //let input_dataY = data.yValue;
-        //let input_dataZ = data.zValue;
-        
-        //https://plot.ly/javascript/bubble-charts/
-        let bubba_trace = {
+let build_3_axis_bubble = {    
+    name: 'bubble'
+    display: 'Bubble Chart',
+    render: function(canvas,data){
+        Plotly.purge(canvas);
+        input_dataX = []
+        input_dataY = []
+        input_dataZ = []
+        for (let item in data.data) {
+            values.push(data.data[item].y_axis);
+            labels.push(item);
+    }
+    let bubba_trace = {
             type:"bubble",
-            text: input_dataX,
+            text: input_dataZ,
             x: input_dataX,
             y: input_dataY,
             mode:'markers',
@@ -24,16 +27,19 @@ function build_3_axis(data) {
         let data2 =[bubba_trace];
             
         let bubba_layout = {
-               title: 'Bubbble Chart Fun',
-               xaxis: { title: "Some sort of Title Variable?" }
+                margin: {
+                t: 10,
+                r: 10,
+                b: 10,
+                l:0
+                }
             };                             
         
     
          Plotly.newPlot("mybubble", data2, bubba_layout);
       
-      
-    
+}
     
   };
-
+widgetRegistry[build_3_axis_bubble.name] = build_3_axis_bubble;
 
