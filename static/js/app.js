2b6cae02fa0e875e50599cfb6a8acb2eadc159db
Andrei Chaplygin <gotacoin@gmail.com>
2018-12-10 18:57:03 -0800

    Added data limits

--- static/js/app.js
+++ static/js/app.js
@@ -35,6 +35,9 @@ function createWidget(widgetSettings) {
     let widgetContent = widget.find('.widget-content')[0];
     $.post('data', JSON.stringify(widgetSettings))
         .done(d => {
+            limitData(d,
+                widgetSettings.top ? +widgetSettings.top.value : 0,
+                widgetSettings.bottom ? +widgetSettings.bottom.value : 0);
             $(widgetContent).data('widgetData', d);
             $(widgetContent).data('widgetSettings', widgetSettings);
             widgetRegistry[widgetSettings.chartType.value].render(widgetContent, d);
@@ -47,3 +50,68 @@ $('.grid-stack').on('gsresizestop', function(event, elem) {
     let data = $(elem).find('.widget-content').data('widgetData');
     widgetRegistry[settings.chartType.value].render($(elem).find('.widget-content')[0], data);
 });
+
+function limitData(data, top, bottom) {
+    top = top || 0;
+    bottom = bottom || 0;
+    // 1. Calculate the cumulative value on every level of the hierarchy (e.g. CA-2014 - 100 and CA-2015 - 200 gives CA-300)
+    let stack = [];
+    stack.push(data.data);
+    while (stack.length > 0) {
+        let item = stack.pop();
+        //Item is at the bottom of the hierarchy - stops everything
+        if (item.hasOwnProperty('y_axis')) {
+            item._cumulative = item.y_axis;
+            item._parent._cumulative = (item._parent._cumulative || 0) + item._cumulative;
+        }
+        //Item still has children - loop through them
+        else {
+            
+            for (let name in item) {
+                if (name.startsWith('_')) {
+                    continue;
+                }
+                let subItem = item[name];
+                subItem._parent = item;
+                stack.push(subItem);                
+            }
+        }
+    }
+    // 2. On every level of hierarchy sort items by cumulative value
+    let queue = [];
+    queue.push(data.data);
+    while (queue.length > 0) {
+        let item = queue.shift();
+        let subItems = Object.entries(item).filter(i => !i[0].startsWith('_')).sort((x, y) => x[1]._cumulative - y[1]._cumulative);
+        let localTop = top || bottom ? top : subItems.length;
+        let localBot = top || bottom ? bottom : subItems.length;
+        for (let i = 0, l = subItems.length; i < l; i++) {
+            if (i < localBot || i >= l - localTop) {
+                //Uncommenct to do it on all levels of the hierarchy
+                //queue.push(subItems[i][1]);
+                continue;
+            }
+            delete item[subItems[i][0]];
+        }
+    }
+    // 3. Cleanup custom fields
+    stack.push(data.data);
+    while (stack.length > 0) {
+        let item = stack.pop();
+        //Item is at the bottom of the hierarchy - stops everything
+        if (item.hasOwnProperty('_parent')) {
+            delete item['_parent']
+        }
+        if (item.hasOwnProperty('_cumulative')) {
+            delete item['_cumulative']
+        }            
+        for (let name in item) {
+            if (name.startsWith('_')) {
+                continue;
+            }
+            let subItem = item[name];
+            subItem._parent = item;
+            stack.push(subItem);                
+        }
+    }
+}
